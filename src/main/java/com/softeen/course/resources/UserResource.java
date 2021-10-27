f6b5b7dd5ee885ebbdcbe6b0fb0a5c8ccf885ac3
AndreSantosWEBDEV <andresantos_webdev@outlook.com>
2021-10-27 18:40:03 -0300

    Primeira class recurso

---
+++ src/main/java/com/softeen/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.softeen.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.softeen.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1, "José", "jose@gmail.com", "1234567", "admin");
+		return ResponseEntity.ok().body(u);
+	}
+}
